1b370df9ec334cedad773bda4035d0862c6d8ea6
Jeff Gore <jgore00@gmail.com>
2018-03-20 15:08:37 -0400

    Completed CSS styling

--- src/index.js
+++ src/index.js
@@ -8,14 +8,13 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kdmVudHVyZWUiLCJhIjoiY2plenRtZWt2MGg5MDJwb
 
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  center: [-74.009, 40.705], // FullStack NY coordinates;
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
-const marker = buildMarker("activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
-marker.addTo(map);
-// new mapboxgl.Marker(markerDomEl).setLngLat([-87.6354, 41.8885]).addTo(map); // [-87.6354, 41.8885] for Chicago
+const fullstack = buildMarker("activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
+fullstack.addTo(map);
 
 const jeffsHouse = buildMarker("hotel", [-74.011813, 40.648814]); // or [-87.6354, 41.8885]
 jeffsHouse.addTo(map);
